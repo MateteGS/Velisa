b3c180f9faf8e3712e255f794ec341b36da3aee5
MateteGS <matetematlala98@gmail.com>
2023-04-03 13:47:05 +0200

    Create hello.js
    My first repository as far as we know

---
+++ Expression/A_Exercuting_code/hello.js
@@ -0,0 +1,8 @@
+console.log("Hello git world");
+// How do you get a percentage
+let a = 50;
+let b = 150;
+let c = 100;
+
+let d = a / b * c
+console.log(`That how much i got on my first project ${d.toFixed(2)}`)
